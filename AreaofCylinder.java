10b8047c8bcd309837634c7be84d6193d808302f
Vinay Divekar <93419500+Vinaydivekar@users.noreply.github.com>
2024-01-25 16:43:33 +0530

    java file

---
+++ AreaofCylinder.java
@@ -0,0 +1,13 @@
+package Assign;
+
+public class AreaofCylinder {
+	public static void main(String[] args) {
+		
+		int radius=10,height=20;
+		
+		System.out.println("Area of Cylinder "+ 2*3.14*radius*(radius+height));
+		System.out.println("Volume of Cylinder "+ 3.14*radius*radius*height);
+		
+	}		
+
+}
